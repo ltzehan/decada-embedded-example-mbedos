79b8c2e1162226f2ecab81f5695c5d3193c56d43
lauleehong <imlauleehong@gmail.com>
2020-10-09 17:00:01 +0800

    fixed header typo

--- src/CryptoEngineV2/crypto_engine_v2.h
+++ src/CryptoEngineV2/crypto_engine_v2.h
@@ -1,9 +1,18 @@
-/*******************************************************************************
- * Copyright (c) 2020, Sensors and IoT Capability Centre (SIOT) at GovTech.
+/*******************************************************************************************************
+ * Copyright (c) 2018-2020 Government Technology Agency of Singapore (GovTech)
+ * SPDX-License-Identifier: Apache-2.0
  *
- * Contributor(s):
- *    Lau Lee Hong  lau_lee_hong@tech.gov.sg
- *******************************************************************************/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ *******************************************************************************************************/
 #ifndef CRYPTO_ENGINE_V2_H
 #define CRYPTO_ENGINE_V2_H
 
