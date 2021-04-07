e983c878d93f6e711972e226311016a47b7a936a
qihao <ltzehan13@gmail.com>
2021-04-07 15:22:53 +0800

    Rerun Doxygen

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -6,7 +6,7 @@ var searchData=
   ['getcertificatesubjectname_23',['GetCertificateSubjectName',['../group__crypto__engine__v3.html#gae47faa01ffcfba69f1054719903c9b95',1,'CryptoEngine']]],
   ['getconfiguredpkinfo_24',['GetConfiguredPkInfo',['../group__se__trustx.html#gae62b8afaf277bfa7536f1db62b4ccc8c',1,'TrustX']]],
   ['getdeviceuid_25',['GetDeviceUid',['../group__device__uuid.html#ga69b32abad345ab3d605a9a920e261c4a',1,'GetDeviceUid(void):&#160;device_uid.cpp'],['../group__device__uuid.html#ga69b32abad345ab3d605a9a920e261c4a',1,'GetDeviceUid(void):&#160;device_uid.cpp']]],
-  ['getmqttstackpointer_26',['GetMqttStackPointer',['../group__decada__manager__v3.html#ga6846e57e38dfd4531df5f84161aaac89',1,'DecadaManager']]],
+  ['getmqttstackpointer_26',['GetMqttStackPointer',['../group__decada__manager.html#ga6846e57e38dfd4531df5f84161aaac89',1,'DecadaManager']]],
   ['getnewdecadapacket_27',['GetNewDecadaPacket',['../group__sensor__profile.html#ga97dda4be4c03e8f1c93c471f48e7d404',1,'SensorProfile']]],
   ['getuserinputstring_28',['GetUserInputString',['../group__boot__manager.html#ga609f7efea8af76e057ed3ef68669d513',1,'GetUserInputString(bool is_hidden):&#160;boot_manager.cpp'],['../group__boot__manager.html#ga609f7efea8af76e057ed3ef68669d513',1,'GetUserInputString(bool is_hidden=false):&#160;boot_manager.cpp']]]
 ];
--- html/search/all_9.js
+++ html/search/all_9.js
@@ -5,5 +5,5 @@ var searchData=
   ['persistent_20storage_36',['Persistent Storage',['../group__persist__store.html',1,'']]],
   ['printheader_37',['PrintHeader',['../group__boot__manager.html#ga67419a147c8cf2822eddb8b92f83b52a',1,'PrintHeader(void):&#160;boot_manager.cpp'],['../group__boot__manager.html#ga67419a147c8cf2822eddb8b92f83b52a',1,'PrintHeader(void):&#160;boot_manager.cpp']]],
   ['printmenu_38',['PrintMenu',['../group__boot__manager.html#gadb6f8df9cf675626c25e365e275394f2',1,'PrintMenu(void):&#160;boot_manager.cpp'],['../group__boot__manager.html#gadb6f8df9cf675626c25e365e275394f2',1,'PrintMenu(void):&#160;boot_manager.cpp']]],
-  ['publish_39',['Publish',['../group__decada__manager__v3.html#gacfe4e1cf5727389f6a5e35e137470c7c',1,'DecadaManager']]]
+  ['publish_39',['Publish',['../group__decada__manager.html#gacfe4e1cf5727389f6a5e35e137470c7c',1,'DecadaManager']]]
 ];
